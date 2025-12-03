f81e6925aea19e06b0145b22e98ba51fae7bd2d1
Yuri Kunde Schlesner <yuriks@yuriks.net>
2025-12-03 11:15:33 -0300

    Day 01 ⭐ get!

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut dial_pos = 50;
+    let mut zero_count = 0;
+
+    for line in io::stdin().lines() {
+        let line = line?;
+        let mut line_chars = line.chars();
+        let movement = match line_chars.next() {
+            Some('L') => -line_chars.as_str().parse::<i32>().unwrap(),
+            Some('R') => line_chars.as_str().parse::<i32>().unwrap(),
+            c => panic!("Unexpected line prefix: {c:?}"),
+        };
+
+        dial_pos = (dial_pos + movement).rem_euclid(100);
+        if dial_pos == 0 {
+            zero_count += 1;
+        }
+    }
+
+    println!("{zero_count}");
+    Ok(())
 }
