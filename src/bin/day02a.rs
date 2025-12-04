a52894fdd9f05b86fbe4f6ebee2d50c72c796991
Yuri Kunde Schlesner <yuriks@yuriks.net>
2025-12-03 21:20:42 -0300

    Day 02 part 2 ⭐ get!

--- src/bin/day02a.rs
+++ src/bin/day02a.rs
@@ -1,21 +1,31 @@
+use std::collections::HashSet;
 use std::io;
 
-// Does not advance if `val` is already a double
-fn advance_to_next_double(mut val: u64) -> u64 {
+// Does not advance if `val` is already a repeating pattern
+fn advance_to_next_double(mut val: u64, n_groups: u32) -> u64 {
     let mut val_digits = val.ilog10() + 1;
-    if val_digits % 2 != 0 {
+    if val_digits % n_groups != 0 {
         // Odd number of digits, expand to next valid even-digited number
-        val_digits += 1;
+        val_digits = val_digits.next_multiple_of(n_groups);
         val = 10u64.pow(val_digits - 1);
     }
 
-    let factor = 10u64.pow(val_digits / 2);
-    let singlet_val = if val / factor >= val % factor {
-        val / factor
-    } else {
-        (val / factor) + 1
-    };
-    singlet_val * factor + singlet_val
+    let factor = 10u64.pow(val_digits / n_groups);
+    let mut best_group = 0;
+    for _ in 0..n_groups {
+        let next_group = val % factor;
+        best_group = if next_group >= best_group {
+            next_group
+        } else {
+            next_group + 1
+        };
+        val /= factor;
+    }
+    let mut result = 0;
+    for _ in 0..n_groups {
+        result = result * factor + best_group;
+    }
+    result
 }
 
 fn main() -> io::Result<()> {
@@ -26,26 +36,45 @@ fn main() -> io::Result<()> {
         }
 
         let mut id_sum = 0u64;
+        let mut id_2group_sum = 0u64;
 
         for range in line.split(',') {
             let (range_begin, range_end) = range.split_once('-').unwrap();
 
-            let mut range_begin = range_begin.parse::<u64>().unwrap();
+            let range_begin = range_begin.parse::<u64>().unwrap();
             let range_end = range_end.parse::<u64>().unwrap();
 
-            loop {
-                range_begin = advance_to_next_double(range_begin);
-                if range_begin > range_end {
-                    break;
+            let mut already_seen = HashSet::new();
+
+            let max_digits = range_end.ilog10() + 1;
+            for n_groups in 2..=max_digits {
+                let mut n_groups_sum = 0;
+                print!("{n_groups}-groups: ");
+                let mut current = range_begin;
+                loop {
+                    current = advance_to_next_double(current, n_groups);
+                    if current > range_end {
+                        break;
+                    }
+                    if already_seen.insert(current) {
+                        n_groups_sum += current;
+                        print!("{current},");
+                    } else {
+                        print!("[{current}],");
+                    }
+                    current += 1;
                 }
-                id_sum += range_begin;
-                print!("{range_begin},");
-                range_begin += 1;
+                println!();
+
+                id_sum += n_groups_sum;
+                if n_groups == 2 {
+                    id_2group_sum += n_groups_sum;
+                }
             }
-            println!();
         }
 
         println!("Sum: {id_sum}");
+        println!("Sum (2-groups only): {id_2group_sum}");
     }
     Ok(())
 }
